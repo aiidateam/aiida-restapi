0e2cf027b41cc97e8bb6a58a013d9a9e9d431318
Chris Sewell <chrisj_sewell@hotmail.com>
2021-06-02 19:40:26 +0200

    ✨ NEW: Add pydantic models

--- aiida_restapi/models.py
+++ aiida_restapi/models.py
@@ -1,24 +1,94 @@
 # -*- coding: utf-8 -*-
-"""Schemas for AiiDA REST API"""
+"""Schemas for AiiDA REST API.
+
+Models in this module mirror those in
+`aiida/backends/djsite/db/models.py` and `aiida/backends/sqlalchemy/models`
+"""
 # pylint: disable=too-few-public-methods
 
-from typing import Optional
+from datetime import datetime
+from typing import List, Optional, Type, TypeVar, ClassVar
 
 from pydantic import BaseModel, Field
 
+from aiida import orm
 
-class User(BaseModel):
-    """AiiDA User model."""
+ModelType = TypeVar("ModelType", bound="AiidaModel")
 
-    id: Optional[int] = Field(description="Unique user id (pk)")
-    first_name: Optional[str] = Field(description="First name of the user")
-    last_name: Optional[str] = Field(description="Last name of the user")
-    institution: Optional[str] = Field(
-        description="Host institution or workplace of the user"
-    )
-    email: str = Field(description="Email address of the user")
+
+class AiidaModel(BaseModel):
+    """A mapping of an AiiDA entity to a pydantic model."""
+
+    _orm_entity: ClassVar[Type[orm.entities.Entity]] = orm.entities.Entity
 
     class Config:
         """The models configuration."""
 
         orm_mode = True
+
+    @classmethod
+    def get_projectable_properties(cls) -> List[str]:
+        """Return projectable properties."""
+        return list(cls.schema()["properties"].keys())
+
+    @classmethod
+    def get_entities(
+        cls: Type[ModelType],
+        *,
+        page_size: Optional[int] = None,
+        page: int = 0,
+        project: Optional[List[str]] = None,
+        order_by: Optional[List[str]] = None,
+    ) -> List[ModelType]:
+        """Return a list of entities (with pagination).
+
+        :param project: properties to project (default: all available)
+        :param page_size: the page size (default: infinite)
+        :param page: the page to return, if page_size set
+        """
+        if project is None:
+            project = cls.get_projectable_properties()
+        else:
+            assert set(cls.get_projectable_properties()).issuperset(
+                project
+            ), f"projection not subset of projectable properties: {project!r}"
+        query = orm.QueryBuilder().append(
+            cls._orm_entity, tag="fields", project=project
+        )
+        if page_size is not None:
+            query.offset(page_size * (page - 1))
+            query.limit(page_size)
+        if order_by is not None:
+            assert set(cls.get_projectable_properties()).issuperset(
+                order_by
+            ), f"order_by not subset of projectable properties: {project!r}"
+            query.order_by({"fields": order_by})
+        return [cls(**result["fields"]) for result in query.dict()]
+
+
+class Comment(AiidaModel):
+    """AiiDA Comment model."""
+
+    _orm_entity = orm.Comment
+
+    id: Optional[int] = Field(description="Unique comment id (pk)")
+    uuid: str = Field(description="Unique comment uuid")
+    ctime: Optional[datetime] = Field(description="Creation time")
+    mtime: Optional[datetime] = Field(description="Last modification time")
+    content: Optional[str] = Field(description="Comment content")
+    dbnode_id: Optional[int] = Field(description="Unique node id (pk)")
+    user_id: Optional[int] = Field(description="Unique user id (pk)")
+
+
+class User(AiidaModel):
+    """AiiDA User model."""
+
+    _orm_entity = orm.User
+
+    id: Optional[int] = Field(description="Unique user id (pk)")
+    email: str = Field(description="Email address of the user")
+    first_name: Optional[str] = Field(description="First name of the user")
+    last_name: Optional[str] = Field(description="Last name of the user")
+    institution: Optional[str] = Field(
+        description="Host institution or workplace of the user"
+    )
---
+++ tests/test_models.py
@@ -0,0 +1,30 @@
+"""Test that all aiida entity models can be loaded loaded into pydantic models."""
+from aiida_restapi import models
+from aiida import orm
+
+
+def replace_dynamic(data: dict) -> dict:
+    """Replace dynamic fields with their type name.
+    """
+    for key in ["id", "uuid", "dbnode_id", "user_id", "mtime", "ctime"]:
+        if key in data:
+            data[key] = type(data[key]).__name__
+    return data
+
+
+def test_comment_get_entities(data_regression):
+    orm_user = orm.User(
+        email="verdi@opera.net", first_name="Giuseppe", last_name="Verdi"
+    ).store()
+    orm_node = orm.Data().store()
+    orm.Comment(orm_node, orm_user, "content").store()
+    py_comments = models.Comment.get_entities(order_by=["id"])
+    data_regression.check([replace_dynamic(c.dict()) for c in py_comments])
+
+
+def test_user_get_entities(data_regression):
+    orm.User(
+        email="verdi@opera.net", first_name="Giuseppe", last_name="Verdi"
+    ).store()
+    py_users = models.User.get_entities(order_by=["id"])
+    data_regression.check([replace_dynamic(c.dict()) for c in py_users])
