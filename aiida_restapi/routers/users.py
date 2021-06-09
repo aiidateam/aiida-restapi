f7e24518274f950f1b36b8a30843dcc434d770cb
Ninad <bhat.ninadmb@gmail.com>
2021-06-09 19:09:11 +1000

    update users endpoint (#21)

--- aiida_restapi/routers/users.py
+++ aiida_restapi/routers/users.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 
 from aiida import orm
 from aiida.cmdline.utils.decorators import with_dbenv
+from aiida.orm.querybuilder import QueryBuilder
 from fastapi import APIRouter, Depends
 
 from aiida_restapi.models import User
@@ -17,19 +18,24 @@ router = APIRouter()
 @with_dbenv()
 async def read_users() -> List[User]:
     """Get list of all users"""
-    return [User.from_orm(u) for u in orm.User.objects.find()]
+    return User.get_entities()
+
+
+@router.get("/users/projectable_properties", response_model=List[str])
+async def get_users_projectable_properties() -> List[str]:
+    """Get projectable properties for users endpoint"""
+
+    return User.get_projectable_properties()
 
 
 @router.get("/users/{user_id}", response_model=User)
 @with_dbenv()
 async def read_user(user_id: int) -> Optional[User]:
     """Get user by id."""
-    orm_user = orm.User.objects.get(id=user_id)
+    qbobj = QueryBuilder()
+    qbobj.append(orm.User, filters={"id": user_id}, project=["**"], tag="user").limit(1)
 
-    if orm_user:
-        return User.from_orm(orm_user)
-
-    return None
+    return qbobj.dict()[0]["user"]
 
 
 @router.post("/users", response_model=User)
