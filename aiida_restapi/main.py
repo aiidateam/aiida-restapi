b30278b892d75c24bb79e647ebcf6cef21e6e6f3
Chris Sewell <chrisj_sewell@hotmail.com>
2021-06-09 11:13:24 +0200

    fix conflict

--- aiida_restapi/main.py
+++ aiida_restapi/main.py
@@ -3,9 +3,10 @@
 from fastapi import FastAPI
 
 from aiida_restapi.graphql import main
-from aiida_restapi.routers import auth, users
+from aiida_restapi.routers import auth, computers, users
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(computers.router)
 app.add_route("/graphql", main.app, name="graphql")
