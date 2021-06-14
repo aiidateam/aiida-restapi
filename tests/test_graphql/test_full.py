55de97cf268200f3c5064c410df1b2e31db13fcc
Chris Sewell <chrisj_sewell@hotmail.com>
2021-06-14 09:12:45 +0200

    add full test

---
+++ tests/test_graphql/test_full.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Simple test for the full schema."""
+from graphene.test import Client
+
+from aiida_restapi.graphql.main import SCHEMA
+
+
+def test_full(create_node, orm_regression):
+    node = create_node(label="node 1")
+    client = Client(SCHEMA)
+    executed = client.execute("{ aiidaVersion node(id: %r) { label } }" % (node.id))
+    orm_regression(executed)
