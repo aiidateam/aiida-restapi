c672ea22c2ce67f4ebcb0d01f88c73ee50e382cc
Ninad Bhat <bhat.ninadmb@gmail.com>
2021-07-26 16:52:27 +1000

    review updates

--- aiida_restapi/routers/nodes.py
+++ aiida_restapi/routers/nodes.py
@@ -9,21 +9,21 @@ except ImportError:  # for Python<3.8
 from aiida.cmdline.utils.decorators import with_dbenv
 from fastapi import APIRouter, Depends, File
 
-from aiida_restapi.models import Node, User
+from aiida_restapi import models
 
 from .auth import get_current_active_user
 
 router = APIRouter()
 
 
-@router.post("/nodes", response_model=Node)
+@router.post("/nodes", response_model=models.Node)
 @with_dbenv()
 async def create_node(
-    node: Node,
-    current_user: User = Depends(
+    node: models.Node,
+    current_user: models.User = Depends(
         get_current_active_user
     ),  # pylint: disable=unused-argument
-) -> Node:
+) -> models.Node:
     """Create new AiiDA node."""
 
     node_dict = node.dict(exclude_unset=True)
@@ -38,29 +38,36 @@ async def create_node(
                 node_type, attributes, node_dict
             )
 
-    return Node.from_orm(orm_object)
+    return models.Node.from_orm(orm_object)
 
 
 @router.post("/singlefiledata")
 @with_dbenv()
 async def create_upload_file(
     upload_file: bytes = File(...),
-    params: Node = Depends(Node.as_form),  # type: ignore # pylint: disable=maybe-no-member
-    current_user: User = Depends(
+    params: models.Node = Depends(models.Node.as_form),  # type: ignore # pylint: disable=maybe-no-member
+    current_user: models.User = Depends(
         get_current_active_user
     ),  # pylint: disable=unused-argument
-) -> Node:
+) -> models.Node:
     """Test function for upload file will be merged with create_node later."""
     node_dict = params.dict(exclude_unset=True, exclude_none=True)
     node_type = node_dict.pop("node_type", None)
     attributes = node_dict.pop("attributes", {})
 
-    entry_point_nodes = metadata.entry_points()["aiida.rest.post"]
+    entry_point_nodes = dict(metadata.entry_points()["aiida.rest.post"])
 
-    for ep_node in entry_point_nodes:
-        if ep_node.name == node_type:
-            orm_object = ep_node.load().create_new_node_with_file(
-                node_type, attributes, node_dict, upload_file
+    try:
+        orm_object = (
+            entry_point_nodes[node_type]
+            .load()
+            .create_new_node_with_file(node_type, attributes, node_dict, upload_file)
+        )
+    except KeyError as exc:
+        raise KeyError(
+            "Entry point '{}' not recognized. Allowed values: {}".format(
+                node_type, list(entry_point_nodes.keys())
             )
+        ) from exc
 
-    return Node.from_orm(orm_object)
+    return models.Node.from_orm(orm_object)
