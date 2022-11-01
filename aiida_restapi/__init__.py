f4f08c6d988f4c788c5e3cca49cdc677f7517a7f
Sebastiaan Huber <mail@sphuber.net>
2022-11-01 16:57:57 +0100

    DevOps: Adopt PEP 621 and use `pyproject.toml`
    PEP 621 introduces the `pyproject.toml` file which is now the
    recommended way for defining a project with its build and install
    dependencies. In addition, it allows to provide configuration for
    various tools, such as tox, instead of using individual files for each
    tool.
    
    The `MANIFEST.in` file is no longer required since the build backend
    `flit` will automatically include all files, except those explicitly
    marked with `[tool.flit.sdist] exclude`.
    
    The description is added to the `__init__.py` of the package and it is
    marked as `dynamic` in the `pyproject.toml` which will automatically
    fetch it from the package docstring.

--- aiida_restapi/__init__.py
+++ aiida_restapi/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-aiida_restapi
-
-AiiDA REST API for data queries and workflow managment.
-"""
+"""AiiDA REST API for data queries and workflow managment."""
 
 __version__ = "0.1.0a0"
 
