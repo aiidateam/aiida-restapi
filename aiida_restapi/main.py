534bdda983974ac6d8ae2d128eadc298e771b102
Sebastiaan Huber <mail@sphuber.net>
2022-11-23 13:50:19 +0100

    Add the `/daemon` endpoint. (#52)
    This new endpoint allows to control the daemon and offers:
    
     * `/daemon/status`: Return the status of the daemon
     * `/daemon/start`: Start the daemon if it is not already
     * `/daemon/stop`: Stop the daemon if it is running

--- aiida_restapi/main.py
+++ aiida_restapi/main.py
@@ -3,11 +3,12 @@
 from fastapi import FastAPI
 
 from aiida_restapi.graphql import main
-from aiida_restapi.routers import auth, computers, groups, nodes, process, users
+from aiida_restapi.routers import auth, computers, daemon, groups, nodes, process, users
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(computers.router)
+app.include_router(daemon.router)
 app.include_router(nodes.router)
 app.include_router(groups.router)
 app.include_router(users.router)
---
+++ aiida_restapi/routers/daemon.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""Declaration of FastAPI router for daemon endpoints."""
+from __future__ import annotations
+
+import typing as t
+
+from aiida.cmdline.utils.decorators import with_dbenv
+from aiida.engine.daemon.client import DaemonException, get_daemon_client
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel, Field
+
+from ..models import User
+from .auth import get_current_active_user
+
+router = APIRouter()
+
+
+class DaemonStatusModel(BaseModel):
+    """Response model for daemon status."""
+
+    running: bool = Field(description="Whether the daemon is running or not.")
+    num_workers: t.Optional[int] = Field(
+        description="The number of workers if the daemon is running."
+    )
+
+
+@router.get("/daemon/status", response_model=DaemonStatusModel)
+@with_dbenv()
+async def get_daemon_status() -> DaemonStatusModel:
+    """Return the daemon status."""
+    client = get_daemon_client()
+
+    if not client.is_daemon_running:
+        return DaemonStatusModel(running=False, num_workers=None)
+
+    response = client.get_numprocesses()
+
+    return DaemonStatusModel(running=True, num_workers=response["numprocesses"])
+
+
+@router.post("/daemon/start", response_model=DaemonStatusModel)
+@with_dbenv()
+async def get_daemon_start(
+    current_user: User = Depends(
+        get_current_active_user
+    ),  # pylint: disable=unused-argument
+) -> DaemonStatusModel:
+    """Start the daemon."""
+    client = get_daemon_client()
+
+    if client.is_daemon_running:
+        raise HTTPException(status_code=400, detail="The daemon is already running.")
+
+    try:
+        client.start_daemon()
+    except DaemonException as exception:
+        raise HTTPException(status_code=500, detail=str(exception)) from exception
+
+    response = client.get_numprocesses()
+
+    return DaemonStatusModel(running=True, num_workers=response["numprocesses"])
+
+
+@router.post("/daemon/stop", response_model=DaemonStatusModel)
+@with_dbenv()
+async def get_daemon_stop(
+    current_user: User = Depends(
+        get_current_active_user
+    ),  # pylint: disable=unused-argument
+) -> DaemonStatusModel:
+    """Stop the daemon."""
+    client = get_daemon_client()
+
+    if not client.is_daemon_running:
+        raise HTTPException(status_code=400, detail="The daemon is not running.")
+
+    try:
+        client.stop_daemon()
+    except DaemonException as exception:
+        raise HTTPException(status_code=500, detail=str(exception)) from exception
+
+    return DaemonStatusModel(running=False, num_workers=None)
