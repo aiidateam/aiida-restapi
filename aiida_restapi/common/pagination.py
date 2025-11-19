e78d7659acaabf19f58a901dd66dfed65c2d5b8a
Edan Bainglass <edan.bainglass@gmail.com>
2025-11-19 13:20:32 +0100

    Add `__all__` in pagination.py to resolve docs issue

--- aiida_restapi/common/pagination.py
+++ aiida_restapi/common/pagination.py
@@ -9,6 +9,8 @@ from aiida import orm  # noqa: F401
 
 from .types import EntityModelType
 
+__all__ = ('PaginatedResults',)
+
 
 class PaginatedResults(pdt.BaseModel, t.Generic[EntityModelType]):
     total: int
