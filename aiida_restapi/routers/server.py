7dae899a990193d120ed3e897430a0c50cfccd6f
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-01 07:47:06 +0100

    Move server endpoints route to server router

--- aiida_restapi/routers/server.py
+++ aiida_restapi/routers/server.py
@@ -1,7 +1,10 @@
 """Declaration of FastAPI application."""
 
+import re
+
 from aiida import __version__ as aiida_version
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.routing import APIRoute
 from pydantic import BaseModel
 
 from aiida_restapi.config import API_CONFIG
@@ -21,7 +24,7 @@ class ServerInfo(BaseModel):
 
 @router.get('/server/info', response_model=ServerInfo)
 async def get_server_info() -> ServerInfo:
-    """Get the API version information"""
+    """Get the API version information."""
     api_version = API_CONFIG['VERSION'].split('.')
     return ServerInfo(
         API_major_version=api_version[0],
@@ -30,3 +33,31 @@ async def get_server_info() -> ServerInfo:
         API_prefix=API_CONFIG['PREFIX'],
         AiiDA_version=aiida_version,
     )
+
+
+class ServerEndpoint(BaseModel):
+    """API endpoint."""
+
+    path: str
+    group: str | None
+    methods: set[str]
+
+
+@router.get('/server/endpoints', response_model=dict[str, list[ServerEndpoint]])
+async def get_server_endpoints(request: Request) -> dict[str, list[ServerEndpoint]]:
+    """List available routes."""
+
+    routes: list[ServerEndpoint] = []
+    for route in request.app.routes:
+        if isinstance(route, APIRoute):
+            full_path = API_CONFIG['PREFIX'] + route.path
+            match = re.search(r'^\/([^\/]+)', route.path)
+            endpoint_group = None if match is None else match.group(1)
+            endpoint = ServerEndpoint(
+                path=full_path,
+                group=endpoint_group,
+                methods=route.methods,
+            )
+            routes.append(endpoint)
+
+    return {'endpoints': routes}
