975b55bc4e1531e889d0197760a9f83795ece6ff
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-01 06:54:27 +0100

    Use pydantic model for server info

--- aiida_restapi/routers/server.py
+++ aiida_restapi/routers/server.py
@@ -1,26 +1,32 @@
 """Declaration of FastAPI application."""
 
-from typing import Any
-
 from aiida import __version__ as aiida_version
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from aiida_restapi.config import API_CONFIG
 
 router = APIRouter()
 
 
-@router.get('/server/info', response_model=dict[str, Any])
-async def get_server_info() -> dict[str, Any]:
-    """Get the API version information"""
-    response = {}
-    api_version = API_CONFIG['VERSION'].split('.')
-    response['API_major_version'] = api_version[0]
-    response['API_minor_version'] = api_version[1]
-    response['API_revision_version'] = api_version[2]
-    # Add Rest API prefix
-    response['API_prefix'] = API_CONFIG['PREFIX']
+class ServerInfo(BaseModel):
+    """API version information."""
 
-    # Add AiiDA version
-    response['AiiDA_version'] = aiida_version
-    return response
+    API_major_version: str
+    API_minor_version: str
+    API_revision_version: str
+    API_prefix: str
+    AiiDA_version: str
+
+
+@router.get('/server/info', response_model=ServerInfo)
+async def get_server_info() -> ServerInfo:
+    """Get the API version information"""
+    api_version = API_CONFIG['VERSION'].split('.')
+    return ServerInfo(
+        API_major_version=api_version[0],
+        API_minor_version=api_version[1],
+        API_revision_version=api_version[2],
+        API_prefix=API_CONFIG['PREFIX'],
+        AiiDA_version=aiida_version,
+    )
