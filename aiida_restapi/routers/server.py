c6c1029004125217ab28eb584bad924cb553f442
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-02 19:26:06 +0100

    Add pydantic field descriptions

--- aiida_restapi/routers/server.py
+++ aiida_restapi/routers/server.py
@@ -4,24 +4,24 @@ from __future__ import annotations
 
 import re
 
+import pydantic as pdt
 from aiida import __version__ as aiida_version
 from fastapi import APIRouter, Request
 from fastapi.routing import APIRoute
-from pydantic import BaseModel
 
 from aiida_restapi.config import API_CONFIG
 
 router = APIRouter()
 
 
-class ServerInfo(BaseModel):
+class ServerInfo(pdt.BaseModel):
     """API version information."""
 
-    API_major_version: str
-    API_minor_version: str
-    API_revision_version: str
-    API_prefix: str
-    AiiDA_version: str
+    API_major_version: str = pdt.Field(description='Major version of the API')
+    API_minor_version: str = pdt.Field(description='Minor version of the API')
+    API_revision_version: str = pdt.Field(description='Revision version of the API')
+    API_prefix: str = pdt.Field(description='Prefix for all API endpoints')
+    AiiDA_version: str = pdt.Field(description='Version of the AiiDA installation')
 
 
 @router.get('/server/info', response_model=ServerInfo)
@@ -37,12 +37,12 @@ async def get_server_info() -> ServerInfo:
     )
 
 
-class ServerEndpoint(BaseModel):
+class ServerEndpoint(pdt.BaseModel):
     """API endpoint."""
 
-    path: str
-    group: str | None
-    methods: set[str]
+    path: str = pdt.Field(description='Path of the endpoint')
+    group: str | None = pdt.Field(description='Group of the endpoint')
+    methods: set[str] = pdt.Field(description='HTTP methods supported by the endpoint')
 
 
 @router.get('/server/endpoints', response_model=dict[str, list[ServerEndpoint]])
