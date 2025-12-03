2d04a9211cf3a0112800344b1d57cb2a47f3791b
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-03 07:30:38 +0100

    Fix test typing and fixture usage

--- tests/test_computers.py
+++ tests/test_computers.py
@@ -1,30 +1,36 @@
 """Test the /computers endpoint"""
 
+from __future__ import annotations
+
+import pytest
 from aiida import orm
+from fastapi.testclient import TestClient
 
 
-def test_get_computer_projectable_properties(client):
+def test_get_computer_projectable_properties(client: TestClient):
     """Test get projectable properties for computer."""
     response = client.get('/computers/projectable_properties')
     assert response.status_code == 200
     assert response.json() == sorted(orm.Computer.fields.keys())
 
 
-def test_get_computers(default_computers, client):  # pylint: disable=unused-argument
+@pytest.mark.usefixtures('default_computers')
+def test_get_computers(client: TestClient):
     """Test listing existing computer."""
     response = client.get('/computers/')
     assert response.status_code == 200
     assert len(response.json()['results']) == 2
 
 
-def test_get_computer(default_computers, client):  # pylint: disable=unused-argument
+def test_get_computer(client: TestClient, default_computers: list[int | None]):
     """Test retrieving a single computer."""
     for comp_id in default_computers:
         response = client.get(f'/computers/{comp_id}')
         assert response.status_code == 200
 
 
-def test_create_computer(client, authenticate):  # pylint: disable=unused-argument
+@pytest.mark.usefixtures('authenticate')
+def test_create_computer(client: TestClient):
     """Test creating a new computer."""
     response = client.post(
         '/computers',
