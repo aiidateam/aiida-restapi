fe91f070aa7d76b78a7ab267cf4dc53e9d1eda5e
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-03 06:36:47 +0100

    Implement server router

--- aiida_restapi/main.py
+++ aiida_restapi/main.py
@@ -1,29 +1,13 @@
 """Declaration of FastAPI application."""
 
 import os
-import typing as t
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import RedirectResponse
 
+from aiida_restapi.config import API_CONFIG
 from aiida_restapi.graphql import main
-from aiida_restapi.routers import auth, computers, daemon, groups, nodes, submit, users
-from aiida_restapi.utils import generate_endpoints_table
-
-
-def generate_endpoints_table_endpoint(app: FastAPI) -> t.Callable[[Request], HTMLResponse]:
-    """Generate an endpoint that lists all registered API routes."""
-
-    def list_endpoints(request: Request) -> HTMLResponse:
-        """Return an HTML table of all registered API routes."""
-        return HTMLResponse(
-            content=generate_endpoints_table(
-                str(request.base_url).rstrip('/'),
-                app.routes,
-            ),
-        )
-
-    return list_endpoints
+from aiida_restapi.routers import auth, computers, daemon, groups, nodes, server, submit, users
 
 
 def create_app() -> FastAPI:
@@ -32,15 +16,28 @@ def create_app() -> FastAPI:
     read_only = os.getenv('AIIDA_RESTAPI_READ_ONLY') == '1'
 
     app = FastAPI()
-    app.include_router(auth.router)
 
-    for module in (computers, daemon, groups, nodes, submit, users):
+    api_router = APIRouter(prefix=API_CONFIG['PREFIX'])
+
+    api_router.include_router(auth.router)
+
+    for module in (computers, daemon, groups, nodes, server, submit, users):
         if read_router := getattr(module, 'read_router', None):
-            app.include_router(read_router)
+            api_router.include_router(read_router)
         if not read_only and (write_router := getattr(module, 'write_router', None)):
-            app.include_router(write_router)
+            api_router.include_router(write_router)
 
-    app.add_route('/graphql', main.app)
-    app.add_route('/', lambda request: generate_endpoints_table_endpoint(app)(request))
+    api_router.add_route(
+        '/graphql',
+        main.app,
+        methods=['POST'],
+    )
+
+    api_router.add_route(
+        '/',
+        lambda _: RedirectResponse(url=api_router.url_path_for('endpoints')),
+    )
+
+    app.include_router(api_router)
 
     return app
