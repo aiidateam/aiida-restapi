f3034cbe011cdf25fcc8ae12a53b8d6f7e7f6cc1
Edan Bainglass <edan.bainglass@gmail.com>
2025-12-28 03:03:41 +0100

    Reorder routes in server endpoints endpoint

--- aiida_restapi/main.py
+++ aiida_restapi/main.py
@@ -19,7 +19,12 @@ def create_app() -> FastAPI:
 
     api_router = APIRouter(prefix=API_CONFIG['PREFIX'])
 
-    for module in (auth, computers, daemon, groups, nodes, querybuilder, server, submit, users):
+    api_router.add_route(
+        '/',
+        lambda _: RedirectResponse(url=api_router.url_path_for('endpoints')),
+    )
+
+    for module in (auth, server, users, computers, groups, nodes, querybuilder, submit, daemon):
         if read_router := getattr(module, 'read_router', None):
             api_router.include_router(read_router)
         if not read_only and (write_router := getattr(module, 'write_router', None)):
@@ -29,11 +34,6 @@ def create_app() -> FastAPI:
         '/graphql',
         main.app,
         methods=['POST'],
-    )
-
-    api_router.add_route(
-        '/',
-        lambda _: RedirectResponse(url=api_router.url_path_for('endpoints')),
     )
 
     app.include_router(api_router)
